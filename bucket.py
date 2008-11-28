346daacda20cdc945ffa6edc7de837daa75f7dca
latrokles <latrokles@gmail.com>
2008-11-27 23:08:40 -0500

    Rewrote bucket procedurally.
    - I think the old approach was a bit too over-engineered for something
      this trivial. Perhaps this looks like one large main method, but it's
      clearly outlined and executed... I could be wrong.
    
    - Began using the Pyglet sprites. They work nicely and now I know that I
      can easily batch the water drops to optimize a bit the drawing.
    
    - I want to switch to the new Pyglet event loop using on_draw() and
      run(), but I definitely won't until I get a chance to read up on them
      more. Last time I did that on a project, I got some weird behavior.
    
    - Still need to implement functions to create the drops and some
      mechanism to keep a general idea of their location on the screen.
    
    - Need to implement collision detection. If I can keep track of the
      drops position, then I can just check if they overlap with the
      player's position... this may be the easiest way to implement things
      for now. I can perfect it later.
    
    - For now I am using a couple of images borrowed from a friend (Thnx
      Don!) for the sprites. I will replace them with a bucket and drops
      later on.
    
    --L.

--- bucket.py
+++ bucket.py
@@ -8,14 +8,74 @@
 #
 
 import applesms
+import pyglet
 from pyglet.gl import *
 from pyglet import window
 
-def update_player():
-	x, y, z = applesms.getcoords()
+def is_there_a_collision(player, object):
+	raise NotImplementedError
+
+
+def create_drops(some_number):
+	raise NotImplementedError
 
 
 def main():
 	"""Game Loop"""
-	pass
+	#Initialize player and game
+	player_img = pyglet.image.load('./assets/at.png')
+	player = pyglet.sprite.Sprite(player_img)
 
+	## Game Window ##
+	main_window = window.Window(width=600, height=500, caption='Bucket V0.2')
+
+
+	## Game Loop ##
+	while not main_window.has_exit:
+		main_window.dispatch_events()
+		glLoadIdentity()
+		glClear(GL_COLOR_BUFFER_BIT)
+		
+		## Create Water Drops ##
+
+		# Randomly Create Water Drops
+		# Need to keep track of their positions somehow...
+
+		## Update Water Drops ##
+		
+		# Update their position 
+
+		## Update player ##
+		window_width, window_height = main_window.get_size()
+		x, y, z = applesms.coords()
+		player_x, player_y = player.position
+
+		if x > 20 and player_x > 0:
+			player_x -= 5
+
+		if x < 20 and (player_x + player.width) < window_width :
+			player_x += 5
+	
+		if y > 20 and player_y > 0:
+			player_y -= 5
+	
+		if y < 20 and player_y < (window_height/2):
+			player_y += 5
+
+		player.set_position(player_x, player_y)
+
+		## Draw the player ##
+		player.draw()
+
+		## Draw Water Drops ##
+
+		## Score ##
+
+		# Check for collision
+		# Update Score or not
+
+		main_window.flip()
+
+
+if __name__=='__main__':
+	main()
