54ea804f679002f6a87669129e99466ea5c1fba4
latrokles <latrokles@gmail.com>
2008-12-02 23:37:27 -0500

    Slowed the rate of water falling.
    - Created a more sensible way to create water drops, or at least
      distribute them.
    
    - Added some code to show the flooding of the room as the player fails
      to catch water drops. It's commented out for now until I get an image
      for the sptrite.
    
    - I know there's something missing here somewhere... but I need to get
      some rest. Will come back to it tomorrow.
    
    --L.

--- bucket.py
+++ bucket.py
@@ -16,16 +16,21 @@ from pyglet import font
 
 def main():
 	"""Game Loop"""
-	#Initialize player and game
+	#Initialize player and game variables
 	player_img = pyglet.image.load('./assets/bucket_md.png')
 	player = pyglet.sprite.Sprite(player_img)
 	ft = font.load('Arial', 20)
-	score = 0
+	score = 0		#player score
 
 	## Keep Track of water drops
 	water_drops_tracker = []
 	water_drops_batch = pyglet.graphics.Batch()
 	water_drop_img = pyglet.image.load('./assets/drop_sm.png')
+
+	missed_drops = 0	#water not caught
+	#water_pool_img = pyglet.image.load('./assets/pool.png')
+	#pool_x = 0
+	#pool = pyglet.sprite.Sprite(water_pool_img, x=pool_x)
 
 	## Game Window ##
 	main_window = window.Window(width=600, height=500, caption='Bucket V0.2')
@@ -38,15 +43,16 @@ def main():
 		glLoadIdentity()
 		glClear(GL_COLOR_BUFFER_BIT)
 		back_img.blit(0, 0)
+
 		## Create Water Drops ##
 		# Random Position for a created drop of water
-		drop_x = random.randint(0, window_width)
-		drop_y = random.randint(window_height, window_height+200)
-		water_drops_tracker.append(pyglet.sprite.Sprite(water_drop_img, x=drop_x, y=drop_y, batch=water_drops_batch))
+		if len(water_drops_tracker) < 20 and (random.randint(0,100)%3) == 0:
+			drop_x = random.randint(0, window_width)
+			drop_y = random.randint(window_height, window_height+200)
+			water_drops_tracker.append(pyglet.sprite.Sprite(water_drop_img, x=drop_x, y=drop_y, batch=water_drops_batch))
 		
 		## Update player ##
 		x, y, z = applesms.coords()
-		#print "(%d, %d, %d)" % (x, y, z )
 		player_x, player_y = player.position
 
 		if x > 20 and player_x > 0:
@@ -56,11 +62,13 @@ def main():
 			player_x += 5
 	
 		player.set_position(player_x, player_y)
+
 		## Update Water Drops ##
 		# Update their position 
 		for each_drop in water_drops_tracker:
 			drop_x, drop_y = each_drop.position
 			each_drop.set_position(drop_x, drop_y-5)
+			
 			# Check for collision
 			if (drop_x > player_x and drop_x < (player_x + player.width)) and (drop_y > player_y and drop_y < player_y + player.height):
 				water_drops_tracker.remove(each_drop)
@@ -70,6 +78,12 @@ def main():
 			elif drop_y < 0:
 				water_drops_tracker.remove(each_drop)
 				each_drop.delete()
+				missed_drops += 1
+
+		## Update and Draw the water accumulated ##
+		#if missed_drops%2 == 0:
+		#	pool_x += 5
+		#pool.draw()
 
 		## Draw the player ##
 		player.draw()
